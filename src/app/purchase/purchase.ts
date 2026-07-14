0e7e953517e937f91970bc951ee0929102dfaa97
Gideon-Magaita <gideonmagaita2020@gmail.com>
2026-07-14 14:14:49 +0300

    some changes

--- src/app/purchase/purchase.ts
+++ src/app/purchase/purchase.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Api } from '../service/api';
 
 @Component({
   selector: 'app-purchase',
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './purchase.html',
   styleUrl: './purchase.css',
 })
-export class Purchase {}
+export class Purchase implements OnInit {
+ constructor(
+  private api:Api,
+  private cdr:ChangeDetectorRef
+ ){}
+
+ ngOnInit(): void {
+   this.fetchProductsAndSuppliers();
+ }
+
+ fetchProductsAndSuppliers():void{
+  this.api.getAllProducts().subscribe({
+    next:(res:any)=>{
+      
+    }
+  })
+ }
+
+}
