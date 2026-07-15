ad9821c0ba4d0b1d1ff5cc86942088b7d87eb059
MAGAITA GIDEON <gideonmagaita2020@gmail.com>
2026-07-15 07:41:20 +0300

    added some pages

--- src/app/purchase/purchase.ts
+++ src/app/purchase/purchase.ts
@@ -15,16 +15,100 @@ export class Purchase implements OnInit {
   private cdr:ChangeDetectorRef
  ){}
 
- ngOnInit(): void {
-   this.fetchProductsAndSuppliers();
- }
 
- fetchProductsAndSuppliers():void{
-  this.api.getAllProducts().subscribe({
-    next:(res:any)=>{
-      
+  products: any[] = []
+  suppliers: any[] = []
+  productId:string = ''
+  supplierId:string = ''
+  description:string = ''
+  quantity:string = ''
+  message:string = ''
+
+
+  ngOnInit(): void {
+    this.fetchProductsAndSuppliers();
+  }
+
+   fetchProductsAndSuppliers():void{
+    this.api.getAllProducts().subscribe({
+      next: (res: any) => {
+        if (res.status === 200) {
+          this.products = res.products;
+          this.cdr.detectChanges();
+        }
+      },
+      error: (error) => {
+        this.showMessage(
+          error?.error?.message ||
+            error?.message ||
+            'Unable to get Products' + error
+        );
+      },
+    });
+
+    this.api.getAllSuppliers().subscribe({
+      next: (res: any) => {
+        if (res.status === 200) {
+          this.suppliers = res.suppliers;
+          this.cdr.detectChanges();
+        }
+      },
+      error: (error) => {
+        this.showMessage(
+          error?.error?.message ||
+            error?.message ||
+            'Unable to get suppliers' + error
+        );
+      },
+    })
+  }
+
+  //Handle form submission
+  handleSubmit():void{
+    if (!this.productId || !this.supplierId || !this.quantity) {
+      this.showMessage("Please fill all fields")
+      return;
     }
-  })
- }
+    const body = {
+      productId: this.productId,
+      supplierId: this.supplierId,
+      quantity:  parseInt(this.quantity, 10),
+      description: this.description
+    }
+
+    this.api.purchaseProduct(body).subscribe({
+      next: (res: any) => {
+        if (res.status === 200) {
+          this.showMessage(res.message)
+          this.resetForm();
+        }
+      },
+      error: (error) => {
+        this.showMessage(
+          error?.error?.message ||
+            error?.message ||
+            'Unable to get purchase a product' + error
+        );
+      },
+    })
+
+  }
+
+
+  resetForm():void{
+    this.productId = '';
+    this.supplierId = '';
+    this.description = '';
+    this.quantity = '';
+  }
+
+
+
+  showMessage(message: string) {
+    this.message = message;
+    setTimeout(() => {
+      this.message = '';
+    }, 4000);
+  }
 
 }
--- src/app/sell/sell.ts
+++ src/app/sell/sell.ts
@@ -1,9 +1,92 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Api } from '../service/api';
 
 @Component({
   selector: 'app-sell',
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './sell.html',
   styleUrl: './sell.css',
 })
-export class Sell {}
+export class Sell implements OnInit{
+
+  constructor(
+    private api:Api,
+    private cdr:ChangeDetectorRef
+  ){}
+
+  products: any[] = []
+  productId:string = ''
+  description:string = ''
+  quantity:string = ''
+  message:string = ''
+
+  ngOnInit(): void {
+    this.fetchProducts();
+  }
+
+  fetchProducts():void{
+    this.api.getAllProducts().subscribe({
+      next: (res: any) => {
+        if (res.status === 200) {
+          this.products = res.products;
+          this.cdr.detectChanges();
+        }
+      },
+      error: (error) => {
+        this.showMessage(
+          error?.error?.message ||
+            error?.message ||
+            'Unable to get Products' + error
+        );
+      },
+    });
+
+  }
+
+  //Handle form submission
+  handleSubmit():void{
+    if (!this.productId || !this.quantity) {
+      this.showMessage("Please fill all fields")
+      return;
+    }
+    const body = {
+      productId: this.productId,
+      quantity:  parseInt(this.quantity, 10),
+      description: this.description
+    }
+
+    this.api.sellProduct(body).subscribe({
+      next: (res: any) => {
+        if (res.status === 200) {
+          this.showMessage(res.message)
+          this.resetForm();
+        }
+      },
+      error: (error) => {
+        this.showMessage(
+          error?.error?.message ||
+            error?.message ||
+            'Unable to sell a product' + error
+        );
+      },
+    })
+
+  }
+
+
+  resetForm():void{
+    this.productId = '';
+    this.description = '';
+    this.quantity = '';
+  }
+
+  showMessage(message: string) {
+    this.message = message;
+    setTimeout(() => {
+      this.message = '';
+    }, 4000);
+  }
+
+}
